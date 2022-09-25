d29451a2f0fe4b668842aeeaa23e3bc71ac5999e
Nicolas <41026986+srezu@users.noreply.github.com>
2022-09-25 19:41:55 -0300

    Update rabbit and dialog

--- Assets/Scripts/Rabbit.cs
+++ Assets/Scripts/Rabbit.cs
@@ -1,18 +1,71 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Rabbit : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float speed;
+    [SerializeField] private float distanceFromDepredator = 1f;
+    [SerializeField] private Transform depredator;
+    [SerializeField] private Animator anim;
+    [SerializeField] private Rigidbody2D rb;
+
+    private bool inIdle;
+
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Vector2.Distance(transform.position, depredator.position) < distanceFromDepredator)
+        {
+            EscapeToRight();
+            inIdle = false;
+        }
+        else 
+        {
+            if(!inIdle)
+                StartCoroutine(IdleCoroutine());
+        }
+
+        anim.SetFloat("vSpeed", rb.velocity.x);
     }
+
+    private void EscapeToRight()
+    {
+        rb.velocity = transform.right * speed;
+
+        Vector3 theScale = transform.localScale;
+        theScale.x = -1;
+        transform.localScale = theScale;
+    }
+
+    private void Idle()
+    {
+        rb.velocity = Vector2.zero;
+        Vector3 theScale = transform.localScale;
+        theScale.x = 1;
+        transform.localScale = theScale;
+    }
+
+    private IEnumerator IdleCoroutine()
+    {
+        rb.velocity = Vector2.zero;
+        Vector3 theScale = transform.localScale;
+        theScale.x = 1;
+        transform.localScale = theScale;
+
+        while (inIdle)
+        {
+            anim.SetBool("Eating", false);
+            yield return new WaitForSeconds(2f);
+            anim.SetBool("Eating",true);
+        }
+
+        anim.SetBool("Eating", false);
+    }
+
+
 }
